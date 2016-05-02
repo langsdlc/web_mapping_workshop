51b49935cfb17ff6d1eb9983a45a211fda7a1ef8
Lindsey Fox <lindsey.c.langsdon@vanderbilt.edu>
2016-05-02 13:27:36 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,15 +2,15 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.k6fj4l3f';
+var mapId = 'vulibrarygis.019hb2al';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'pk.eyJ1IjoidnVsaWJyYXJ5Z2lzIiwiYSI6ImFaN2JkSlUifQ.Dl2sTO1mGKK7MCd1ViGPnQ';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
 L.mapbox.accessToken = accessToken;
-var map = L.mapbox.map('map', mapId);
+var map = L.mapbox.map('map', vulibrarygis.019hb2al);
 
 // Set the initial view of the map to the whole US
 map.setView([39, -96], 4);
